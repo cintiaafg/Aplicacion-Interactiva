66a200602a4c4aa3960337ff93aeab8742460b66
cintiaafg <cintia.flores8361@alumnos.udg.mx>
2026-09-20 17:35:56 -0600

    Programación de botones

---
+++ Assets/codigos/LoadScene.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour
+{
+public void IniciarJuego()
+    {
+        SceneManager.LoadScene("BotonIniciar");
+    }
+}
